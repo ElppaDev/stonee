19334276f2a59208bc1df6147e0b1cccb718240f
nswin <sa_8001@naver.com>
2020-12-10 01:05:19 +0900

    2020-12-10
    2020-12-10 update

---
+++ jh_js.js
@@ -0,0 +1,14 @@
+function openinstall(key){
+    switch(key){
+        case 1 : location.href = "https://open.kakao.com/o/gmxvhp6b";
+        break;
+        case 2 : location.href = "https://korelppa.github.io/ee/down";
+        break;
+        case 3 : location.href = "https://www.shiqiee.com/app6.html";
+        break;
+        case 4 : location.href = "http://shiqiee.com/tg/m/index_en.html";
+        break;
+        case 5 : location.href = "https://www.shiqiee.com/app4.html";
+        break;
+    }
+}
